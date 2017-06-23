b3f3d04e9b61f563d47e9411de9897c52766eb7a
Yogesh Kumar <kumary@brocade.com>
2017-06-23 18:17:55 +0530

    Added Heading

---
+++ src/Heading.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Heading()
+{
+    return(<h1 className="heading">Birthday Calendar</h1>);
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import BdayCalendar from './BdayCalendar';
+import Heading from './Heading';
 import '../styles/index.scss';
 
 const container=document.getElementById("container");
-ReactDOM.render(<BdayCalendar/>, container);
+function DisplayCalendar()
+{
+    return(
+        <div>
+            <Heading/>
+            <BdayCalendar/>
+        </div>
+    )
+}
+ReactDOM.render(<DisplayCalendar/>, container);
